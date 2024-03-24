9ea74cad387e404b9011dbde1a09d3b6370adcfa
Stanislas Lange <git@slange.me>
2024-03-24 23:37:53 +0100

    Complete challenge #3a: Single-Node Broadcast

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use std::io;
+use std::io::{self};
 
 #[derive(Serialize, Deserialize)]
 struct Message<B> {
@@ -12,6 +12,7 @@ struct Message<B> {
 struct GenericBody {
     #[serde(rename = "type")]
     msg_type: String,
+    msg_id: Option<usize>,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -27,9 +28,12 @@ struct EchoBody {
 struct InitBody {
     #[serde(rename = "type")]
     msg_type: String,
-    msg_id: Option<usize>,
     in_reply_to: Option<usize>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    msg_id: Option<usize>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     node_id: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     node_ids: Option<Vec<String>>,
 }
 
@@ -42,13 +46,54 @@ struct GenerateBody {
     in_reply_to: Option<usize>,
 }
 
+#[derive(Serialize, Deserialize)]
+struct BroadcastBody {
+    #[serde(rename = "type")]
+    msg_type: String,
+    msg_id: Option<usize>,
+    in_reply_to: Option<usize>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    message: Option<usize>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct ReadBody {
+    #[serde(rename = "type")]
+    msg_type: String,
+    msg_id: Option<usize>,
+    in_reply_to: Option<usize>,
+    messages: Option<Vec<usize>>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct TopologyBody {
+    #[serde(rename = "type")]
+    msg_type: String,
+    msg_id: Option<usize>,
+    in_reply_to: Option<usize>,
+    // topology: Option<Topology>, but we don't care for now
+}
+
+#[derive(Serialize, Deserialize)]
+struct ErrorBody {
+    #[serde(rename = "type")]
+    msg_type: String,
+    code: usize,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    in_reply_to: Option<usize>,
+}
+
 fn main() {
     let mut node_id = String::new();
     let mut id_idx = 0;
+    let mut msg_idx = 1;
+    let mut broadcast_messages: Vec<usize> = Vec::new();
 
     loop {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
+
+        eprintln!("input: {}", input);
 
         let input_msg: Message<GenericBody> = serde_json::from_str(&input).unwrap();
 
@@ -61,7 +106,7 @@ fn main() {
                     dest: input_msg.src,
                     body: EchoBody {
                         msg_type: "echo_ok".to_string(),
-                        msg_id: input_msg.body.msg_id,
+                        msg_id: Some(msg_idx),
                         in_reply_to: input_msg.body.msg_id,
                         echo: input_msg.body.echo,
                     },
@@ -69,6 +114,7 @@ fn main() {
 
                 let output = serde_json::to_string(&output_msg).unwrap();
                 println!("{}", output);
+                eprintln!("output: {}", output);
             }
             "init" => {
                 let input_msg: Message<InitBody> = serde_json::from_str(&input).unwrap();
@@ -90,6 +136,7 @@ fn main() {
 
                 let output = serde_json::to_string(&output_msg).unwrap();
                 println!("{}", output);
+                eprintln!("output: {}", output);
             }
             "generate" => {
                 let input_msg: Message<GenerateBody> = serde_json::from_str(&input).unwrap();
@@ -103,17 +150,87 @@ fn main() {
                     body: GenerateBody {
                         msg_type: "generate_ok".to_string(),
                         id: Some(id),
-                        msg_id: input_msg.body.msg_id,
+                        msg_id: Some(msg_idx),
                         in_reply_to: input_msg.body.msg_id,
                     },
                 };
 
                 let output = serde_json::to_string(&output_msg).unwrap();
                 println!("{}", output);
+                eprintln!("output: {}", output);
             }
+            "broadcast" => {
+                let input_msg: Message<BroadcastBody> = serde_json::from_str(&input).unwrap();
+
+                broadcast_messages.push(input_msg.body.message.unwrap());
+
+                let output_msg = Message {
+                    src: input_msg.dest,
+                    dest: input_msg.src,
+                    body: BroadcastBody {
+                        msg_type: "broadcast_ok".to_string(),
+                        msg_id: Some(msg_idx),
+                        in_reply_to: input_msg.body.msg_id,
+                        message: None,
+                    },
+                };
+
+                let output = serde_json::to_string(&output_msg).unwrap();
+                println!("{}", output);
+                eprintln!("output: {}", output);
+            }
+            "read" => {
+                let input_msg: Message<ReadBody> = serde_json::from_str(&input).unwrap();
+
+                let output_msg = Message {
+                    src: input_msg.dest,
+                    dest: input_msg.src,
+                    body: ReadBody {
+                        msg_type: "read_ok".to_string(),
+                        msg_id: Some(msg_idx),
+                        in_reply_to: input_msg.body.msg_id,
+                        messages: Some(broadcast_messages.clone()),
+                    },
+                };
+
+                let output = serde_json::to_string(&output_msg).unwrap();
+                println!("{}", output);
+                eprintln!("output: {}", output);
+            }
+            "topology" => {
+                let input_msg: Message<TopologyBody> = serde_json::from_str(&input).unwrap();
+
+                let output_msg = Message {
+                    src: input_msg.dest,
+                    dest: input_msg.src,
+                    body: TopologyBody {
+                        msg_type: "topology_ok".to_string(),
+                        in_reply_to: input_msg.body.msg_id,
+                        msg_id: Some(msg_idx),
+                    },
+                };
+
+                let output = serde_json::to_string(&output_msg).unwrap();
+                println!("{}", output);
+                eprintln!("output: {}", output);
+            }
+
             _ => {
-                eprint!("unknown message type: {}\n", input_msg.body.msg_type);
+                let output_msg = Message {
+                    src: input_msg.dest,
+                    dest: input_msg.src,
+                    body: ErrorBody {
+                        msg_type: "error".to_string(),
+                        code: 12,
+                        in_reply_to: input_msg.body.msg_id,
+                    },
+                };
+
+                let output = serde_json::to_string(&output_msg).unwrap();
+                println!("{}", output);
+                eprintln!("output: {}", output);
             }
         }
+        msg_idx += 1;
     }
 }
